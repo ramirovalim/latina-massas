1d64af7debc22c7ca634567a0aad81cb69be0b2b
Ramiro Valim <ramirotuk@gmail.com>
2024-11-01 20:20:36 -0300

    fix: unused imports 2 :,)

--- pages/about/AboutLayout.tsx
+++ pages/about/AboutLayout.tsx
@@ -1,6 +1,5 @@
 import { Footer } from "@/components/ui/footer";
 import { Header } from "@/components/ui/header";
-import { NavLinks } from "@/components/ui/nav-link";
 
 export function AboutLayout(props: { children: React.ReactNode }) {
   const { children } = props;
